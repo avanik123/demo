c2ff577829202246f78420266a18b28141632416
komal <avaniksavani77@gmail.com>
2021-04-12 18:08:19 +0530

    add changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index.as_view(), name='index'),
-    path('product', views.ProductList.as_view(), name='productlist'),
-    # path('products', views.ListProduct.as_view(), name='listproduct'),
-    # path('product/<int:pk>', views.ProductDetail.as_view(), name='productdetail'),
+    path('', views.LoginTemplate.as_view(), name='login'),
+    path('index', views.HomeTemplate.as_view(), name='index'),
+    path('product', views.ProductTemplate.as_view(), name='product'),
+
+    path('products', views.ListProduct.as_view(), name='listproduct'),
+    path('products/<int:pk>', views.ProductDetail.as_view(), name='productdetail'),
+    path('createproduct', views.Product.as_view(), name='createproduct'),
+    path('updateproduct/<int:id>', views.ProductPutUpdate.as_view(), name='updateproduct'),
+    path('deleteproduct/<int:id>', views.ProductDelete.as_view(), name='deleteproduct'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,29 +1,87 @@
 from django.shortcuts import render
-from .models import *
-from .serializers import *
-from rest_framework import generics
 from django.views.generic import TemplateView
+from myapp import models
+from myapp import serializers
+from rest_framework import generics
+from rest_framework import status
+from django.contrib import messages
+from rest_framework.response import Response
 
 # Create your views here.
-class index(TemplateView):
+class LoginTemplate(TemplateView):
+    template_name = 'login.html'
+
+
+class HomeTemplate(TemplateView):
     template_name = 'index.html'
 
 
-class ProductList(TemplateView):
+class ProductTemplate(TemplateView):
+    queryset = models.Product.objects.all()
     template_name = 'product.html'
-    model = Product
+    model = models.Product
 
     def get_context_data(self, **kwargs):
-        context = super(ProductList, self).get_context_data(**kwargs)
-        context['products'] = Product.objects.all()
+        context = super(ProductTemplate, self).get_context_data(**kwargs)
+        context['products'] = models.Product.objects.all()
         return context
 
 
-# class ListProduct(generics.ListCreateAPIView):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
+class ListProduct(generics.ListCreateAPIView):
+    queryset = models.Product.objects.all()
+    serializer_class = serializers.ProductSerializer
 
 
-# class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Product.objects.all()
+    serializer_class = serializers.ProductSerializer
+
+
+class Product(generics.CreateAPIView):
+    serializer_class = serializers.ProductSerializer
+    model = models.Product
+
+    def create(self, request, *args, **kwargs):
+        try:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            return Response({"status": 200, "message": 'created successfully.'}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({"status": 400, "message": "Fail to create"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProductPutUpdate(generics.UpdateAPIView):
+    serializer_class = serializers.ProductSerializer
+    model = models.Product
+    lookup_field = 'id'
+    http_method_names = ['put']
+
+    def get_queryset(self):
+        return self.model.objects.all(id)
+
+    def update(self, request, *args, **kwargs):
+        try:
+            response = super(ProductPutUpdate, self).update(self, request, *args, **kwargs)
+            print(response)
+            return Response({'success': True, 'data': response.data})
+        except Exception as e:
+            print(e)
+            return Response({'message': format(e.args[-1]), 'success': False})
+
+
+class ProductDelete(generics.DestroyAPIView):
+    serializer_class = serializers.ProductSerializer
+    model = models.Product
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        return self.model.objects.all()
+
+    def delete(self, request, *args, **kwargs):
+        try:
+            response = super(ProductDelete, self).delete(request, *args, **kwargs)
+            return Response({"status": True, "message": "Product deleted successfully."}, status=status.HTTP_200_OK)
+        except Exception as e:
+            print(e)
+            return Response({"status": False, "message": "Fail to delete product"}, status=status.HTTP_400_BAD_REQUEST)
