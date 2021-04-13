de0e135ebbcbbbc1297237d7982750b7806a9d2d
komal <avaniksavani77@gmail.com>
2021-04-13 20:23:44 +0530

    add changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('listproducts1', views.ProductDatatableView.as_view(), name='listproducts1'),
     path('', views.LoginTemplate.as_view(), name='login'),
     path('index', views.HomeTemplate.as_view(), name='index'),
     path('product', views.ProductTemplate.as_view(), name='product'),
 
-    path('products', views.ListProduct.as_view(), name='listproduct'),
     path('products/<int:pk>', views.ProductDetail.as_view(), name='productdetail'),
     path('createproduct', views.Product.as_view(), name='createproduct'),
     path('updateproduct/<int:id>', views.ProductPutUpdate.as_view(), name='updateproduct'),
